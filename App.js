9a2f354fc02d846c0a3d0472b305bae1c6fb8da4
yongyeonkim-dev <seyeon.dev@gmail.com>
2021-09-08 15:22:57 +0900

    수업 2주차
    component, style, useState

--- App.js
+++ App.js
@@ -1,21 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  Button,
+} from "react-native";
 
 export default function App() {
+  const [buttonTitle, setButtonTitle] = useState("Press Me");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView
+      contentContainerStyle={styles.container}
+      style={{ padding: 10 }}
+    >
+      <View style={styles.title}>
+        <Text style={{ fontSize: 25, fontWeight: 600 }}>
+          Hello React Native
+        </Text>
+        <Text style={{ fontSize: 15, color: "blue" }}>
+          만나서 반갑습니다 :)
+        </Text>
+      </View>
+      <View style={styles.sentenceContainer}>
+        <Text style={styles.sentence}>First sentence</Text>
+        <Text style={styles.sentence}>Second sentence</Text>
+      </View>
+      <Image style={styles.image} source={require("./cat-icon.png")} />
+      <Image style={styles.image} source={require("./cat-icon.png")} />
+      <Image style={styles.image} source={require("./cat-icon.png")} />
+      <Button
+        title={buttonTitle}
+        onPress={() => {
+          setButtonTitle("Ouch!");
+        }}
+      ></Button>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    alignSelf: "stretch",
+    textAlign: "center",
+    backgroundColor: "yellow",
+    padding: 10,
+    borderRadius: 10,
+    marginBottom: 10,
+  },
+  image: { width: 200, height: 200, marginBottom: 10 },
+  sentenceContainer: {
+    alignSelf: "stretch",
+  },
+  sentence: {
+    fontSize: 16,
+    padding: 10,
+    backgroundColor: "gray",
+    color: "white",
+    borderRadius: 10,
+    marginBottom: 10,
   },
 });
