4c53ce1288643c5460e04c67968799127e7054f3
yongyeonkim-dev <seyeon.dev@gmail.com>
2021-10-13 14:33:12 +0900

    Lab3: 구구단
    배열로 구구단 만들기

--- App.js
+++ App.js
@@ -1,140 +1,27 @@
 import React from "react";
-import { StyleSheet, View, ImageBackground } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 
-import { Audio } from "expo-av";
+let L = [];
 
-async function play00() {
-  console.log("Loading Sound");
-  const s = await Audio.Sound.createAsync(require("./assets/note00.m4a"));
-  console.log("Playing Sound");
-  s.sound.playAsync().catch((error) => {});
-}
-
-async function play02() {
-  const s = await Audio.Sound.createAsync(require("./assets/note02.m4a"));
-  s.sound.playAsync().catch((error) => {});
-}
-
-async function play04() {
-  const s = await Audio.Sound.createAsync(require("./assets/note04.m4a"));
-  s.sound.playAsync().catch((error) => {});
-}
-
-async function play05() {
-  const s = await Audio.Sound.createAsync(require("./assets/note05.m4a"));
-  s.sound.playAsync().catch((error) => {});
-}
-
-async function play07() {
-  const s = await Audio.Sound.createAsync(require("./assets/note07.m4a"));
-  s.sound.playAsync().catch((error) => {});
-}
-
-async function play09() {
-  const s = await Audio.Sound.createAsync(require("./assets/note09.m4a"));
-  s.sound.playAsync().catch((error) => {});
-}
-
-async function play11() {
-  const s = await Audio.Sound.createAsync(require("./assets/note11.m4a"));
-  s.sound.playAsync().catch((error) => {});
-}
-
-async function play12() {
-  const s = await Audio.Sound.createAsync(require("./assets/note12.m4a"));
-  s.sound.playAsync().catch((error) => {});
+for (let i = 2; i <= 9; i++) {
+  for (let j = 1; j <= 9; j++) {
+    L.push(
+      <Text>
+        {i} x {j} = {i * j}
+      </Text>
+    );
+  }
 }
 
 export default function App() {
   return (
-    <View style={styles.body}>
-      <ImageBackground
-        style={styles.background}
-        resizeMode="stretch"
-        source={require("./assets/keyboard.png")}
-      >
-        <View
-          style={styles.keyStyle}
-          onTouchStart={(event) => {
-            event.target.style.opacity = 1;
-            play00();
-          }}
-          onTouchEnd={(event) => {
-            event.target.style.opacity = 0;
-          }}
-        />
-        <View
-          style={styles.keyStyle}
-          onTouchStart={(event) => {
-            play02();
-            event.target.style.opacity = 1;
-          }}
-          onTouchEnd={(event) => {
-            event.target.style.opacity = 0;
-          }}
-        />
-        <View
-          style={styles.keyStyle}
-          onTouchStart={(event) => {
-            play04();
-            event.target.style.opacity = 1;
-          }}
-          onTouchEnd={(event) => {
-            event.target.style.opacity = 0;
-          }}
-        />
-        <View
-          style={styles.keyStyle}
-          onTouchStart={(event) => {
-            play05();
-            event.target.style.opacity = 1;
-          }}
-          onTouchEnd={(event) => {
-            event.target.style.opacity = 0;
-          }}
-        />
-        <View
-          style={styles.keyStyle}
-          onTouchStart={(event) => {
-            play07();
-            event.target.style.opacity = 1;
-          }}
-          onTouchEnd={() => {
-            event.target.style.opacity = 0;
-          }}
-        />
-        <View
-          style={styles.keyStyle}
-          onTouchStart={(event) => {
-            play09();
-            event.target.style.opacity = 1;
-          }}
-          onTouchEnd={(event) => {
-            event.target.style.opacity = 0;
-          }}
-        />
-        <View
-          style={styles.keyStyle}
-          onTouchStart={(event) => {
-            play11();
-            event.target.style.opacity = 1;
-          }}
-          onTouchEnd={(event) => {
-            event.target.style.opacity = 0;
-          }}
-        />
-        <View
-          style={styles.keyStyle}
-          onTouchStart={(event) => {
-            play12();
-            event.target.style.opacity = 1;
-          }}
-          onTouchEnd={(event) => {
-            event.target.style.opacity = 0;
-          }}
-        />
-      </ImageBackground>
-    </View>
+    <ScrollView
+      contentContainerStyle={styles.contentContainer}
+      style={styles.body}
+    >
+      {L}
+    </ScrollView>
   );
 }
 
@@ -144,15 +31,8 @@ const styles = StyleSheet.create({
     marginTop: 30,
   },
 
-  background: {
-    width: "100%",
-    height: "100%",
-  },
-
-  keyStyle: {
-    flex: 1,
-    margin: 5,
-    backgroundColor: "rgba(100, 100, 100, 0.2)",
-    opacity: 0,
+  contentContainer: {
+    margin: 20,
+    marginTop: 0,
   },
 });
