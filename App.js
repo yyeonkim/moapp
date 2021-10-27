243179d04efc5bd9a20375650f041d8aac143154
yyeonkim <seyeon.dev@gmail.com>
2021-10-27 14:57:48 +0900

    Make button to call setNum if user press it

--- App.js
+++ App.js
@@ -7,17 +7,30 @@ export default function App() {
   const [num, setNum] = useState(1);
 
   return (
-    <View style={styles.body}>
-      <Dice num={num} />
+    <View style={{ flex: 1, marginTop: 100 }}>
+      <Text style={styles.text}>Dice</Text>
+      <View style={{ flexDirection: "row", justifyContent: "center" }}>
+        <Dice num={num} />
+      </View>
+      <View style={styles.button}>
+        <Button
+          title="Roll the dice"
+          onPress={() => setNum(Math.floor(Math.random() * 6 + 1))}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  body: {
-    flex: 1,
-    marginTop: 40,
-    justifyContent: "space-around",
-    alignItems: "center",
+  text: {
+    textAlign: "center",
+    fontSize: 20,
+    margin: 5,
+  },
+
+  button: {
+    marginHorizontal: 100,
+    marginVertical: 30,
   },
 });
