40be7b51f0caf2926fc72745145ff61d9f6099d3
yyeonkim <seyeon.dev@gmail.com>
2021-11-03 15:19:47 +0900

    Children of component

--- App.js
+++ App.js
@@ -1,27 +1,15 @@
-import React, { useState } from "react";
-import { StyleSheet, View, Text, Button, TouchableOpacity } from "react-native";
-
-import Dice from "./components/Dice";
+import React from "react";
+import { StyleSheet, Text, Button, View } from "react-native";
+import BoxGroup from "./components/BoxGroup";
 
 export default function App() {
-  const [num, setNum] = useState(1);
-
-  const getRandom = () => {
-    return Math.floor(Math.random() * 6 + 1);
-  };
-
   return (
     <View style={styles.body}>
-      <Text style={styles.text}>Dice</Text>
-      <Text style={styles.sum}>{num}</Text>
-      <TouchableOpacity
-        style={styles.dices}
-        onPress={() => {
-          setNum(getRandom());
-        }}
-      >
-        <Dice num={num} />
-      </TouchableOpacity>
+      <BoxGroup>
+        <Text style={styles.text}>ABCD</Text>
+        <Text style={styles.text}>123</Text>
+        <Button title="OK" />
+      </BoxGroup>
     </View>
   );
 }
@@ -29,29 +17,14 @@ export default function App() {
 const styles = StyleSheet.create({
   body: {
     flex: 1,
-    justifyContent: "center",
+    marginTop: 40,
+    alignItems: "center",
   },
 
   text: {
-    textAlign: "center",
-    fontSize: 20,
-    marginBottom: 10,
-  },
-
-  sum: {
-    textAlign: "center",
-    fontSize: 50,
-    fontWeight: "bold",
-    marginBottom: 10,
-  },
-
-  dices: {
-    flexDirection: "row",
-    justifyContent: "center",
-  },
-
-  button: {
-    marginHorizontal: 100,
-    marginVertical: 30,
+    backgroundColor: "wheat",
+    fontSize: 30,
+    margin: 2,
+    padding: 2,
   },
 });
