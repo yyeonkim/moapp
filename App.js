9d847bd95ebb81ddb81181be9e812ce98c0e292e
yyeonkim <seyeon.dev@gmail.com>
2021-11-10 14:21:01 +0900

    Simple clock
    useEffect를 사용하여 시계 만들기

--- App.js
+++ App.js
@@ -1,33 +1,56 @@
-import React from "react";
-import { StyleSheet, Text, Button, View } from "react-native";
-import BoxGroup from "./components/BoxGroup";
-import Mult from "./components/Mult";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [hour, setHour] = useState("");
+  const [minute, setMinute] = useState("");
+  const [second, setSecond] = useState("");
+  const [meridiem, setMeridiem] = useState("");
+
+  const updateTime = () => {
+    const now = new Date();
+    //Set minute and second
+    setMinute((now.getMinutes() < 10 && "0") + now.getMinutes());
+    setSecond((now.getSeconds() < 10 && "0") + now.getSeconds());
+
+    // Set hour and meridiem
+    if (now.getHours() < 12) {
+      setMeridiem("am");
+      if (now.getHours() === 0) setHour(12);
+      else setHour("0" + now.getHours());
+    } else {
+      setMeridiem("pm");
+      if (now.getHours() === 24) setHour(12);
+      else setHour("0" + (now.getHours() - 12));
+    }
+  };
+
+  useEffect(() => {
+    setInterval(updateTime, 1000);
+  }, []);
+
   return (
-    <View style={{ paddingTop: 30 }}>
-      <BoxGroup>
-        <Mult />
-      </BoxGroup>
-      <BoxGroup>
-        <Mult />
-        <Mult />
-      </BoxGroup>
+    <View style={styles.body}>
+      <Text style={[styles.text, { fontWeight: "bold" }]}>
+        {hour}:{minute}
+      </Text>
+      <Text style={styles.text}>
+        :{second} {meridiem}
+      </Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   body: {
-    flex: 1,
+    backgroundColor: "wheat",
+    flexDirection: "row",
     marginTop: 40,
-    alignItems: "center",
+    padding: 10,
+    textAlign: "center",
   },
 
   text: {
-    backgroundColor: "wheat",
-    fontSize: 30,
-    margin: 2,
-    padding: 2,
+    fontSize: 50,
   },
 });
