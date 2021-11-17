d7b7726f47785ebbbbf7641f8807d4dc6a3270bd
yyeonkim <seyeon.dev@gmail.com>
2021-11-17 15:06:19 +0900

    5 day forcast

--- App.js
+++ App.js
@@ -3,52 +3,37 @@ import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   const API_KEY = "4009dd27be67a419448e41926efeeeb5";
-  const [weatherObj, setWeatherObj] = useState({});
-
-  const getWeather = (data) => {
-    const {
-      name,
-      sys: { country },
-      weather,
-      main: { temp },
-      wind: { speed },
-    } = data;
-
-    setWeatherObj({
-      name,
-      country,
-      weather: weather[0].main,
-      temp,
-      icon: `http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`,
-      speed,
-    });
-  };
+  const [weatherList, setWeatherList] = useState([]);
 
   useEffect(() => {
     fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=${API_KEY}&units=metric`
+      `https://api.openweathermap.org/data/2.5/forecast?q=Seoul&appid=${API_KEY}&units=metric`
     )
       .then((response) => {
         return response.json();
       })
       .then((data) => {
-        getWeather(data);
+        setWeatherList(data.list);
       })
       .catch((error) => {
         console.log(error);
       });
   });
 
-  const { name, country, temp, weather, speed, icon } = weatherObj;
-
   return (
     <View style={styles.body}>
-      <Text>City: {name}</Text>
-      <Text>Country: {country}</Text>
-      <Text>Temperature: {temp}</Text>
-      <Text>Weather: {weather}</Text>
-      <Text>Wind: {speed}</Text>
-      <Image style={styles.icon} source={{ uri: icon }} />
+      {weatherList.map((item) => (
+        <View style={styles.weather}>
+          <Image
+            style={styles.icon}
+            source={{
+              uri: `https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`,
+            }}
+          />
+          <Text>{item.main.temp}</Text>
+          <Text>{item.weather[0].description}</Text>
+        </View>
+      ))}
     </View>
   );
 }
@@ -56,11 +41,23 @@ export default function App() {
 const styles = StyleSheet.create({
   body: {
     marginTop: 40,
-    padding: 10,
+    padding: 20,
+  },
+
+  weather: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 10,
+  },
+
+  text: {
+    marginRight: 20,
   },
 
   icon: {
-    width: 100,
-    height: 100,
+    backgroundColor: "wheat",
+    width: 40,
+    height: 40,
+    marginRight: 20,
   },
 });
