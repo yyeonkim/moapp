06a5e282a2d5ba356def603663759d585c6fbd60
yyeonkim <seyeon.dev@gmail.com>
2021-11-17 14:43:42 +0900

    Current weather
    API 사용하여 날씨 가져오기

--- App.js
+++ App.js
@@ -1,56 +1,66 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
-  const [hour, setHour] = useState("");
-  const [minute, setMinute] = useState("");
-  const [second, setSecond] = useState("");
-  const [meridiem, setMeridiem] = useState("");
+  const API_KEY = "4009dd27be67a419448e41926efeeeb5";
+  const [weatherObj, setWeatherObj] = useState({});
 
-  const updateTime = () => {
-    const now = new Date();
-    //Set minute and second
-    setMinute((now.getMinutes() < 10 && "0") + now.getMinutes());
-    setSecond((now.getSeconds() < 10 && "0") + now.getSeconds());
+  const getWeather = (data) => {
+    const {
+      name,
+      sys: { country },
+      weather,
+      main: { temp },
+      wind: { speed },
+    } = data;
 
-    // Set hour and meridiem
-    if (now.getHours() < 12) {
-      setMeridiem("am");
-      if (now.getHours() === 0) setHour(12);
-      else setHour("0" + now.getHours());
-    } else {
-      setMeridiem("pm");
-      if (now.getHours() === 24) setHour(12);
-      else setHour("0" + (now.getHours() - 12));
-    }
+    setWeatherObj({
+      name,
+      country,
+      weather: weather[0].main,
+      temp,
+      icon: `http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`,
+      speed,
+    });
   };
 
   useEffect(() => {
-    setInterval(updateTime, 1000);
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=${API_KEY}&units=metric`
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        getWeather(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   });
+
+  const { name, country, temp, weather, speed, icon } = weatherObj;
 
   return (
     <View style={styles.body}>
-      <Text style={[styles.text, { fontWeight: "bold" }]}>
-        {hour}:{minute}
-      </Text>
-      <Text style={styles.text}>
-        :{second} {meridiem}
-      </Text>
+      <Text>City: {name}</Text>
+      <Text>Country: {country}</Text>
+      <Text>Temperature: {temp}</Text>
+      <Text>Weather: {weather}</Text>
+      <Text>Wind: {speed}</Text>
+      <Image style={styles.icon} source={{ uri: icon }} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   body: {
-    backgroundColor: "wheat",
-    flexDirection: "row",
     marginTop: 40,
     padding: 10,
-    textAlign: "center",
   },
 
-  text: {
-    fontSize: 50,
+  icon: {
+    width: 100,
+    height: 100,
   },
 });
