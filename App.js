bb7468ba394f1f30d0459bb56594d53a764b8ede
yyeonkim <seyeon.dev@gmail.com>
2021-12-01 14:10:53 +0900

    lab2: Add memo UI

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View, TextInput } from "react-native";
 import CalendarPicker from "react-native-calendar-picker";
 
 export default function App() {
   const [date, setDate] = useState("");
   const [date1, setDate1] = useState("");
+  const [expense, setExpense] = useState("");
+  const [memo, setMemo] = useState("");
 
   const onDateChange = (date) => {
     setDate(date.format("YYYYMMDD"));
     setDate1(date.format("MMMM DD, YYYY"));
   };
+
   return (
     <View style={styles.container}>
       <CalendarPicker onDateChange={onDateChange} />
       <View style={styles.box}>
-        <Text>Date: {date}</Text>
-        <Text style={styles.text}>{date1}</Text>
+        <Text style={styles.dateText}>{date1}</Text>
+        <Text style={styles.row}>
+          <Text style={styles.text}>Expense</Text>
+          <TextInput
+            style={styles.expense}
+            keyboardType="numeric"
+            onChangeText={setExpense}
+          />
+          <Button title="SAVE" />
+        </Text>
+        <TextInput
+          style={styles.memo}
+          placeholder="Memo"
+          onChangeText={setMemo}
+        />
       </View>
     </View>
   );
@@ -31,8 +47,32 @@ const styles = StyleSheet.create({
     margin: 20,
   },
 
-  text: {
+  dateText: {
     fontSize: 20,
     marginVertical: 10,
+  },
+
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+
+  text: {
+    fontSize: 16,
+  },
+
+  expense: {
+    flex: 1,
+    borderBottomWidth: 1,
+    marginHorizontal: 10,
+    fontSize: 16,
+    color: "blue",
+  },
+
+  memo: {
+    borderBottomWidth: 1,
+    fontSize: 16,
+    marginTop: 10,
+    color: "blue",
   },
 });
