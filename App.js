3c8f97dd58dddecde5e6c2ff071f6b23e1bd788b
yyeonkim <seyeon.dev@gmail.com>
2021-12-01 15:16:38 +0900

    lab5: Save and load data

--- App.js
+++ App.js
@@ -1,18 +1,90 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import React, { useState } from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import HomeScreen from "./src/pages/HomeScreen";
-import MemoScreen from "./src/pages/MemoScreen";
+import { Button, View, TextInput, StyleSheet, Text } from "react-native";
 
-const Stack = createStackNavigator();
+const phoneBook = [
+  { id: "1", name: "Ewha", phone: 1234 },
+  { id: "2", name: "June", phone: 3352 },
+  { id: "3", name: "Jane", phone: 2334 },
+  { id: "4", name: "Nick", phone: 1231 },
+];
 
 export default function App() {
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [refresh, setRefresh] = useState(true);
+
+  const addItem = () => {
+    phoneBook.push({ name, phone });
+    setRefresh((current) => !current);
+  };
+
+  const loadData = async () => {
+    const value = await AsyncStorage.getItem("phoneBook");
+    phoneBook = JSON.parse(value);
+    setRefresh((current) => !current);
+  };
+
+  const saveDate = async () => {
+    await AsyncStorage.setItem("phoneBook", JSON.stringify(phoneBook));
+  };
+
+  const lists = [];
+  for (let i = 0; i < phoneBook.length; i++) {
+    const item = (
+      <View key={phoneBook.id} style={{ flexDirection: "row" }}>
+        <Text style={styles.text}>{phoneBook[i].name}</Text>
+        <Text style={styles.text}>{phoneBook[i].phone}</Text>
+      </View>
+    );
+    lists.push(item);
+  }
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Memo" component={MemoScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={{ marginTop: 30 }}>
+      <View style={styles.title}>
+        <Text style={{ fontSize: 40 }}>Phone Book</Text>
+        <Button title="SAVE" onPress={saveDate} />
+        <Button title="LOAD" onPress={loadData} />
+      </View>
+      <View style={styles.inputBox}>
+        <TextInput style={styles.input} onChangeText={setName} />
+        <TextInput style={styles.input} onChangeText={setPhone} />
+        <Button title="ADD" onPress={addItem} />
+      </View>
+      {lists}
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  title: {
+    margin: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+
+  text: {
+    fontSize: 20,
+    borderWidth: 1,
+    borderColor: "gray",
+    flex: 1,
+    padding: 5,
+    margin: 2,
+  },
+
+  inputBox: {
+    margin: 10,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+
+  input: {
+    fontSize: 20,
+    borderWidth: 1,
+    flex: 1,
+    padding: 5,
+    margin: 5,
+  },
+});
